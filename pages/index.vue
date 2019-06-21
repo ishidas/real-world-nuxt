8b342e0caec724fe477684bfad0919a012b386d5
Sawako Sagliano <ssagli@uw.edu>
2019-06-21 09:26:21 -0700

    real world nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,39 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        real-world-nuxt
-      </h1>
-      <h2 class="subtitle">
-        My nuxt project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
-    </div>
+  <div>
+    <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
-
 <script>
-import Logo from '~/components/Logo.vue'
+import EventCard from '@/components/EventCard'
+import { mapState } from 'vuex'
 
 export default {
   components: {
-    Logo
+    EventCard
+  },
+  head() {
+    return {
+      title: 'Event Listing'
+    }
+  },
+  computed: mapState({
+    events: state => state.events.events
+  }),
+  async fetch({ store, error }) {
+    try {
+      await store.dispatch('events/fetchEvents')
+    } catch (err) {
+      error({
+        statusCOde: 503,
+        message: 'Unable to fetch at this time. please try again.'
+      })
+    }
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
